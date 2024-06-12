9e10596c492df50d5bde17ad16e2396c9545eb71
victor-pi-almeida <victorres.almeida@gmail.com>
2024-06-12 10:12:57 +0100

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,23 +2,24 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    "prettier"
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh',"@typescript-eslint", "react", "prettier"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "@typescript-eslint", "react", "prettier"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/no-unused-vars": "off",
   },
   settings: {
-    'import/resolver': {
-      'typescript': {}
-    }
-  }
-}
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+};
